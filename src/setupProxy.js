bf18efaf34bbfdd24d22f6cb888520cfb735bbc2
LangWenqi <497379037@qq.com>
2020-03-09 16:28:59 +0800

    规避域名
    规避域名

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,7 @@
 const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
-  // icldh8k88eqe4o1uolg0b9upaj
-	// vvitdm1njqgdbi3bqevrm8sgi8
   app.use(proxy('/api', { 
-		target: 'http://192.168.137.74:9401',
+		target: 'http://192.168.xxx.xx:xxxx',
 		changeOrigin: true,
 		onProxyReq: (proxyReq,req,res) => {
 			proxyReq.setHeader('cookie', 'PHPSESSID=vvitdm1njqgdbi3bqevrm8sgi8')
